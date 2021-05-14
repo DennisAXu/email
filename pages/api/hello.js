6a82d03f1cf62077b740378a3ec13407c56205e2
Dennis Xu <dennisaxu@gmail.com>
2021-05-14 18:11:52 -0400

    400

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-//   res.json(req)
+  res.statusCode = 400
+  //res.json(req)
   res.json(JSON.stringify(req.query), JSON.stringify(req.body));
 }
