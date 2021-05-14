a29600acc417c7cf6701923a59b4709eb061ad8a
Dennis Xu <dennisaxu@gmail.com>
2021-05-14 18:24:03 -0400

    yes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { MemClient } from "@mem-labs/mem-node";
+
+const memClient = new MemClient({
+  apiKey: "3c37816e-c46c-4a55-8b99-f9c137d5c65c"
+});
+
 export default (req, res) => {
   res.statusCode = 200
   //res.json(req)
-  res.send(req);
+  memClient.createMem({
+    content: req.subject
+  })
+  res.send("success");
 }
